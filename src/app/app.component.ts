c91731b4fdc71d444916ec184b12d86fcfe86ab1
liuguangjie <534712355@qq.com>
2020-10-26 13:19:34 +0800

    'homework'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Trapeze } from './interface';
+import {open} from 'shapefile';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public isCollapsed = false;
   public markerArray: Array<Trapeze> = [];
   public marker: Trapeze;
+  public geoArray = {
+    type: 'FeatureCollection',
+    features: []
+  };
+  public getGeoJson = {
+    type: 'FeatureCollection',
+    features: []
+  };
   constructor() {
+  }
+
+  public ngOnInit(): void {
   }
 
   public getRandomNum(min, max) {
@@ -35,5 +47,33 @@ export class AppComponent {
 
   public mapRightEvent() {
     this.markerArray = [];
+  }
+
+  public updateFilename(event) {
+    const that = this;
+    console.log(event);
+    const file = event.target.files[0];
+    const render = new FileReader();
+    render.onload = (e) => {
+      console.log(e);
+      open(e.target.result)
+        .then(source => source.read()
+          .then(function log(result) {
+            if (result.done) { return; }
+            const array = result.value.geometry;
+            that.geoArray.features.push(result.value);
+            setTimeout(() => {
+              that.getGeoJson = that.geoArray;
+            }, 1000);
+            return source.read().then(log);
+          }));
+    };
+    render.readAsArrayBuffer(file);
+  }
+
+  public handleData(data: Array<any>) {
+    for (let a = 0; a < data.length; a++) {
+
+    }
   }
 }
